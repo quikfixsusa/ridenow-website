170a191d7c31f2a2b2cf8742e9a9f5e9248c2fc5
Luis Garcia <lg5967133@gmail.com>
2026-07-22 13:18:32 +0200

    added redirect to credentials to app links

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: [
-        'rlp6blss-3001.uks1.devtunnels.ms',
-        'localhost:3001'
-      ],
+      allowedOrigins: ['rlp6blss-3001.uks1.devtunnels.ms', 'localhost:3001'],
     },
+  },
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        destination: '/api/.well-known/apple-app-site-association',
+        permanent: false,
+      },
+      {
+        source: '/.well-known/assetlinks',
+        destination: '/api/.well-known/assetlinks',
+        permanent: false,
+      },
+    ];
   },
 };
 
