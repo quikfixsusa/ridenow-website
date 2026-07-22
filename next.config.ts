12ff579f60fe5ad88cfacf1b64a0f19344bfda3b
Luis Garcia <lg5967133@gmail.com>
2026-07-22 16:05:54 +0200

    added redirects to auth actions

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,20 @@ const nextConfig: NextConfig = {
       allowedOrigins: ['rlp6blss-3001.uks1.devtunnels.ms', 'localhost:3001'],
     },
   },
+  async redirects() {
+    return [
+      {
+        source: '/auth/reset-password',
+        destination: '/auth/actions',
+        permanent: false,
+      },
+      {
+        source: '/auth/verify-email',
+        destination: '/auth/actions',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
