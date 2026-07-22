26a69e3c2fc635efabd65cc580492cd8d9f89d48
Luis Garcia <lg5967133@gmail.com>
2026-07-22 13:27:23 +0200

    change routes deep link from api to .well-known

--- next.config.ts
+++ next.config.ts
@@ -6,20 +6,6 @@ const nextConfig: NextConfig = {
       allowedOrigins: ['rlp6blss-3001.uks1.devtunnels.ms', 'localhost:3001'],
     },
   },
-  async redirects() {
-    return [
-      {
-        source: '/.well-known/apple-app-site-association',
-        destination: '/api/.well-known/apple-app-site-association',
-        permanent: false,
-      },
-      {
-        source: '/.well-known/assetlinks',
-        destination: '/api/.well-known/assetlinks',
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
